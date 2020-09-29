5fe19e1058f21f828d51a14dd7789f811fbc42e5
castielqiu <castielqiu@gmail.com>
2020-09-29 10:14:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab1_map524_dps924_zhiweiqiu/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab1_map524_dps924_zhiweiqiu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
